3a1441c5650c49697733d239bf4ff72197efa96b
Alex Karasyov <alex.karasyov@gmail.com>
2015-03-23 14:16:53 -0400

    initiating Foundation Apps with Angular

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,173 @@
-//gulp and plugins
-var gulp			= require('gulp'),
-	del 			= require('del'),
-	beep			= require('beepbeep'),
-	concat			= require('gulp-concat'),
-	stylish			= require('jshint-stylish'),
-	jshint 			= require('gulp-jshint'),
-	sass			= require('gulp-ruby-sass'),
-	sourcemaps		= require('gulp-sourcemaps'),
-	jshint			= require('gulp-jshint'),
-	plumber			= require('gulp-plumber'),
-	gutil			= require('gulp-util'),
-	connect			= require('gulp-connect'),
-	livereload		= require('gulp-livereload'),
-	sequence		= require('run-sequence');
+// FOUNDATION FOR APPS TEMPLATE GULPFILE
+// -------------------------------------
+// This file processes all of the assets in the "client" folder, combines them with the Foundation for Apps assets, and outputs the finished files in the "build" folder as a finished app.
 
+// 1. LIBRARIES
+// - - - - - - - - - - - - - - -
 
-//error handling
-var onError = function (err) {
-	beep();
-	gutil.log(gutil.colors.red(err));
-};
+var gulp     = require('gulp'),
+    $        = require('gulp-load-plugins')(),
+    rimraf   = require('rimraf'),
+    sequence = require('run-sequence'),
+    router   = require('./bower_components/foundation-apps/bin/gulp-dynamic-routing');
 
-//path variables
-var DEST_PATH = 'dest/',
-	DEV_PATH  = 'dev/';
+// 2. FILE PATHS
+// - - - - - - - - - - - - - - -
 
+var paths = {
+  assets: [
+    './client/**/*.*',
+    '!./client/templates/**/*.*',
+    '!./client/assets/{scss,js}/**/*.*'
+  ],
+  // Sass will check these folders for files when you use @import.
+  sass: [
+    'client/assets/scss',
+    'bower_components/foundation-apps/scss'
+  ],
+  // These files include Foundation for Apps and its dependencies
+  foundationJS: [
+    'bower_components/fastclick/lib/fastclick.js',
+    'bower_components/viewport-units-buggyfill/viewport-units-buggyfill.js',
+    'bower_components/tether/tether.js',
+    'bower_components/hammerjs/hammer.js',
+    'bower_components/angular/angular.js',
+    'bower_components/angular-animate/angular-animate.js',
+    'bower_components/angular-ui-router/release/angular-ui-router.js',
+    'bower_components/foundation-apps/js/vendor/**/*.js',
+    'bower_components/foundation-apps/js/angular/**/*.js',
+    '!bower_components/foundation-apps/js/angular/app.js'
+  ],
+  // These files are for your app's JavaScript
+  appJS: [
+    'client/assets/js/app.js'
+  ]
+}
 
+// 3. TASKS
+// - - - - - - - - - - - - - - -
+
+// Cleans the build directory
 gulp.task('clean', function(cb) {
-	return del( DEST_PATH + '*', cb );
+  rimraf('./build', cb);
 });
 
-gulp.task('html', function() {
-	return gulp.src( DEV_PATH + '**.html' )
-		.pipe(gulp.dest( DEST_PATH ));
+// Copies everything in the client folder except templates, Sass, and JS
+gulp.task('copy', function() {
+  return gulp.src(paths.assets, {
+    base: './client/'
+  })
+    .pipe(gulp.dest('./build'))
+  ;
 });
 
-gulp.task('sass', function() {
-    return sass( DEV_PATH + 'scss', { sourcemap: true }) 
-    	.on('error', function (err) {onError(err);})
-    	.pipe(sourcemaps.write())
-    	.pipe(gulp.dest( DEST_PATH + 'css'));
+// Compiles Sass
+gulp.task('sass', function () {
+  return gulp.src('client/assets/scss/app.scss')
+    .pipe($.sass({
+      includePaths: paths.sass,
+      outputStyle: 'nested',
+      errLogToConsole: true
+    }))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie 10']
+    }))
+    .pipe(gulp.dest('./build/assets/css/'))
+  ;
 });
 
-gulp.task('scripts', function() {
-	return gulp.src( DEV_PATH + 'js/**' )
-		.pipe(plumber({errorHandler: onError }))
-		// .pipe(jshint())
-  // 		.pipe(jshint.reporter(stylish))
-  // 		.pipe(jshint.reporter('fail'))
-		.pipe(gulp.dest( DEST_PATH + 'js'));
+// Compiles and copies the Foundation for Apps JavaScript, as well as your app's custom JS
+gulp.task('uglify', function(cb) {
+  // Foundation JavaScript
+  gulp.src(paths.foundationJS)
+    .pipe($.uglify()
+      .on('error', function (e) {
+        console.log(e);
+      }))
+    .pipe($.concat('foundation.js'))
+    .pipe(gulp.dest('./build/assets/js/'))
+  ;
+
+  // App JavaScript
+  gulp.src(paths.appJS)
+    .pipe($.uglify()
+      .on('error', function(e) {
+        console.log(e);
+      }))
+    .pipe($.concat('app.js'))
+    .pipe(gulp.dest('./build/assets/js/'))
+  ;
+
+  cb();
 });
 
-gulp.task('images', function() {
-	return gulp.src( DEV_PATH + 'img/**' )
-		.pipe(plumber({errorHandler: onError }))
-		.pipe(gulp.dest( DEST_PATH + 'img'));
+// Copies your app's page templates and generates URLs for them
+gulp.task('copy:templates', function() {
+  return gulp.src('./client/templates/**/*.html')
+    .pipe(router({
+      path: 'build/assets/js/routes.js',
+      root: 'client'
+    }))
+    .pipe(gulp.dest('./build/templates'))
+  ;
 });
 
-gulp.task('connect', function() {
-	return connect.server({
-		root: DEST_PATH,
-		port: 9001,
-		livereload: true
-	});
+// Compiles the Foundation for Apps directive partials into a single JavaScript file
+gulp.task('copy:foundation', function(cb) {
+  gulp.src('bower_components/foundation-apps/js/angular/components/**/*.html')
+    .pipe($.ngHtml2js({
+      prefix: 'components/',
+      moduleName: 'foundation',
+      declareModule: false
+    }))
+    .pipe($.uglify())
+    .pipe($.concat('templates.js'))
+    .pipe(gulp.dest('./build/assets/js'))
+  ;
+
+  // Iconic SVG icons
+  gulp.src('./bower_components/foundation-apps/iconic/**/*')
+    .pipe(gulp.dest('./build/assets/img/iconic/'))
+  ;
+
+  cb();
+})
+
+// Starts a test server, which you can view at http://localhost:8080
+gulp.task('server', function() {
+  gulp.src('./build')
+    .pipe($.webserver({
+      port: 8080,
+      host: 'localhost',
+      fallback: 'index.html',
+      livereload: true,
+      open: true
+    }))
+  ;
 });
 
-gulp.task('watch', function() {
-	livereload.listen();
-	gulp.watch(DEV_PATH + '**.html', ['html']);
-	gulp.watch( DEV_PATH + 'scss/**', ['sass']);
-	gulp.watch( DEV_PATH + 'img/**', ['images']);
-	gulp.watch( DEV_PATH + 'js/**', ['scripts']);
-	gulp.watch (DEST_PATH + '**').on('change', livereload.changed)
+// Builds your entire app once, without starting a server
+gulp.task('build', function(cb) {
+  sequence('clean', ['copy', 'copy:foundation', 'sass', 'uglify'], 'copy:templates', function() {
+    console.log("Successfully built.");
+    cb();
+  });
 });
 
-gulp.task('dev', ['clean'], function(callback) {
-	sequence(
-		'clean',
-		['html','sass','scripts','images'],
-		'connect',
-		'watch',
-		callback
-		);	
-});
+// Default task: builds your app, starts a server, and recompiles assets when they change
+gulp.task('default', function () {
+  // Run the server after the build
+  sequence('build', 'server');
 
-gulp.task('default', ['dev']);
+  // Watch Sass
+  gulp.watch(['./client/assets/scss/**/*', './scss/**/*'], ['sass']);
+
+  // Watch JavaScript
+  gulp.watch(['./client/assets/js/**/*', './js/**/*'], ['uglify']);
+
+  // Watch static files
+  gulp.watch(['./client/**/*.*', '!./client/templates/**/*.*', '!./client/assets/{scss,js}/**/*.*'], ['copy']);
+
+  // Watch app templates
+  gulp.watch(['./client/templates/**/*.html'], ['copy:templates']);
+});
--- readme.md
+++ readme.md
@@ -1,26 +1,56 @@
-# Quiver Web Portal
-***
+# Quiver Enterprise Portal
 
-## Overview
 
 **Quiver Web Portal** for Enteprise Users.
 
-***
-
-##Requirements
-
-* `node -v 0.12.0` (or latest) [http://nodejs.org](http://nodejs.org)
-* 
-
-***
-##Libraries
+Build on [Zurb Foundation for Apps](http://foundation.zurb.com/apps/) template.
 
 
 
 ***
-##Getting Started
 
-Follow these instructions to get started with this repository.
+## Requirements
 
-1. Navigate to the directory in Terminal.
-2. Run `npm install` to install all dependencies.
+You'll need the following software installed to get started.
+
+  - [Node.js](http://nodejs.org): Use the installer for your OS.
+  - [Git](http://git-scm.com/downloads): Use the installer for your OS.
+    - Windows users can also try [Git for Windows](http://git-for-windows.github.io/).
+  - [Gulp](http://gulpjs.com/) and [Bower](http://bower.io): Run `npm install -g gulp bower`
+    - Depending on how Node is configured on your machine, you may need to run `sudo npm install -g gulp bower` instead, if you get an error with the first command.
+
+***
+## Get Started
+
+1. Clone this repository.
+
+	```
+	git clone http://stash.quiver.zone/projects/QWP/repos/quiver-web-portal/
+	```
+
+2. Change into the directory.
+
+	```
+	cd quiver-web-portal
+	```
+
+3. Install the dependencies. If you're running Mac OS or Linux, you may need to run 	`sudo npm install` instead, depending on how your machine is configured.
+
+	```
+	npm install
+	bower install
+	```
+
+3. While you're working on your project, run:
+
+	```
+	npm start
+	```
+
+	This will compile the Sass and assemble your Angular app. **Now go to `localhost:8080` in your browser to see it in action.** When you change any file in the `client` folder, the appropriate Gulp task will run to build new files.
+
+4. To run the compiling process once, without watching any files, use the `build` command.
+
+	```
+	npm start build
+	```
