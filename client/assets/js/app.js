ab4bfa7eea9b399708eec9f700b121c268b482e3
Alex Karasyov <alex.karasyov@gmail.com>
2015-03-25 00:07:59 -0400

    wip: user page

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('enterprise-portal', [
+  var app = angular.module('enterprise-portal', [
     'ui.router',
     'ngAnimate',
 
@@ -30,5 +30,20 @@
   function run() {
     FastClick.attach(document.body);
   }
+
+  app.controller('UserController', ['$scope', function($scope){
+    $scope.user = {
+      "_id": "<hex key>",
+      "username": "elonmusk",
+      "type": "enterprise-user", // "enterprise-user" "enterprise-admin" "system-admin" 
+      "email": "elon@teslamotors.com",
+      "fullName": "Elon Musk",
+      "description": "The real Iron Man. This is the user description. Up to 140 characters",
+      "title": "Iron Man",
+      "department": "The Avengers",
+      "phone": "555-123-1122",
+      "avatar": "images/avatar.jpg"
+    };
+  }]);
 
 })();
