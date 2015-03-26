ee70f29f8c713b53685bf8d1d5fe7e4500529f0d
Alex Karasyov <alex.karasyov@gmail.com>
2015-03-25 23:09:52 -0400

    admin page

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -89,4 +89,49 @@
     }
   }]);
 
+  app.controller('AdminController', ['$scope', function($scope){
+    $scope.enteprises = {
+      "enteprises": [
+        {
+          "companyName": "Tesla Motors, Inc.",
+          "username": "teslamotors",
+          "logo": "images/logo.jpg",
+          "administrators": [
+            {
+              "name": "Elon Musk",
+              "title": "Executive",
+              "username": "elonmusk",
+              "avatar": "images/avatar.jpg"
+            },
+            {
+              "name": "Nikola Tesla",
+              "title": "Research & Development",
+              "username": "nikola",
+              "avatar": "images/avatar.jpg"
+            }
+          ]
+        },
+        {
+          "companyName": "Space X",
+          "username": "teslamotors",
+          "logo": "images/logo.jpg",
+          "administrators": [
+            {
+              "name": "Elon Musk",
+              "title": "Executive",
+              "username": "elonmusk",
+              "avatar": "images/avatar.jpg"
+            },
+            {
+              "name": "Alber Einstein",
+              "title": "Research & Development",
+              "username": "albert",
+              "avatar": "images/avatar.jpg"
+            }
+          ]
+        }
+      ]
+    }
+  }]);
+
 })();
