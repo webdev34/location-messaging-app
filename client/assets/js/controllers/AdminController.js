b5da4c52e4b91b8aafa920bc62656697f12f83f9
Alex Karasyov <alex.karasyov@gmail.com>
2015-03-27 17:18:41 -0400

    admin http

--- client/assets/js/controllers/AdminController.js
+++ client/assets/js/controllers/AdminController.js
@@ -1,49 +1,55 @@
 (function() {
   'use strict';
 
-  angular.module('enterprise-portal').controller('AdminController', ['$scope', function($scope){
-    $scope.enteprises = {
-      "enteprises": [
-        {
-          "companyName": "Tesla Motors, Inc.",
-          "username": "teslamotors",
-          "logo": "images/logo.jpg",
-          "administrators": [
-            {
-              "name": "Elon Musk",
-              "title": "Executive",
-              "username": "elonmusk",
-              "avatar": "images/avatar.jpg"
-            },
-            {
-              "name": "Nikola Tesla",
-              "title": "Research & Development",
-              "username": "nikola",
-              "avatar": "images/avatar.jpg"
-            }
-          ]
-        },
-        {
-          "companyName": "Space X",
-          "username": "teslamotors",
-          "logo": "images/logo.jpg",
-          "administrators": [
-            {
-              "name": "Elon Musk",
-              "title": "Executive",
-              "username": "elonmusk",
-              "avatar": "images/avatar.jpg"
-            },
-            {
-              "name": "Alber Einstein",
-              "title": "Research & Development",
-              "username": "albert",
-              "avatar": "images/avatar.jpg"
-            }
-          ]
-        }
-      ]
-    }
+  angular.module('enterprise-portal').controller('AdminController', ['$scope','$http', function($scope, $http){
+    // $scope.enteprises = {
+    //   "enteprises": [
+    //     {
+    //       "companyName": "Tesla Motors, Inc.",
+    //       "username": "teslamotors",
+    //       "logo": "images/logo.jpg",
+    //       "administrators": [
+    //         {
+    //           "name": "Elon Musk",
+    //           "title": "Executive",
+    //           "username": "elonmusk",
+    //           "avatar": "images/avatar.jpg"
+    //         },
+    //         {
+    //           "name": "Nikola Tesla",
+    //           "title": "Research & Development",
+    //           "username": "nikola",
+    //           "avatar": "images/avatar.jpg"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       "companyName": "Space X",
+    //       "username": "teslamotors",
+    //       "logo": "images/logo.jpg",
+    //       "administrators": [
+    //         {
+    //           "name": "Elon Musk",
+    //           "title": "Executive",
+    //           "username": "elonmusk",
+    //           "avatar": "images/avatar.jpg"
+    //         },
+    //         {
+    //           "name": "Alber Einstein",
+    //           "title": "Research & Development",
+    //           "username": "albert",
+    //           "avatar": "images/avatar.jpg"
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // }
+
+    $http.get('data/admin.json').
+    success(function(data) {
+      $scope.enterprises = data;
+    });
+
   }]);
 
  
