7a69744ad9716c26e0443a5fa7fcda44b0cd49f1
Alex Karasyov <alex.karasyov@gmail.com>
2015-04-20 09:39:50 -0400

    wip: refactoring messages and models

--- client/app/common/user-side-bar.directive.js
+++ client/app/common/user-side-bar.directive.js
@@ -6,8 +6,7 @@
   	.directive('userSideBar', function () {
 	    return {
 	      restrict: "E",
-	      //template: "<div>Here I am to save the day</div>"
-	      templateUrl: '/app/shared/user-side-bar.directive.html'
+	      templateUrl: '/app/common/user-side-bar.directive.html'
 	    };
 	  });
 
---
+++ client/app/messages/MessageDetailController.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+  	.module('enterprise-portal')
+  	.controller('MessageDetailController', ['$scope', '$http', function($scope, $http) {
+    
+    		
+		    $http.get('assets/data/message-detail.json').
+			    success(function(data) {
+			      $scope.message = data;
+			    });
+
+  }]);
+ 
+
+})();
