8a1e4be795232e2d9ff4cac86413a4d525b1d8d7
Alex Karasyov <alex.karasyov@gmail.com>
2015-04-22 03:25:49 -0400

    clean up

--- client/app/messages/new/messages-new.js
+++ client/app/messages/new/messages-new.js
@@ -4,10 +4,10 @@
   angular.module('messages.new', [])
   	.controller('NewMessageCtrl', ['$scope', '$http', function($scope, $http){
     
-	    $scope.test = "Hello world";
 
 	    $scope.createNewMessage = function(newMessage) {
 	    	alert(newMessage.content);
+
 	    }
   	}]);
  
