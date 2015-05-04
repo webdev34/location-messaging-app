795a371630ece08c53e9a590da86eefd32dd8696
Alex Karasyov <alex.karasyov@gmail.com>
2015-05-04 17:07:46 -0400

    enterprise controller cleanup

--- client/app/enterprise/profile/enterprise-profile.js
+++ client/app/enterprise/profile/enterprise-profile.js
@@ -2,16 +2,27 @@
   'use strict';
 
   angular.module('enterprise.profile', [
-    'enterprise.profile.edit'
+    
     ])
   	.config(['$stateProvider', function ($stateProvider) {
   		$stateProvider
   			.state('enterprise.profile', {
-				url: '/',
-				templateUrl: '/app/enterprise/profile/enterprise-profile.tmpl.html',
-				controller: 'EnterpriseCtrl as enterpriseCtrl'
-  			});
-  	}]);
+				  url: '/',
+				  templateUrl: '/app/enterprise/profile/enterprise-profile.tmpl.html',
+				  controller: 'EnterpriseCtrl as enterpriseCtrl'
+  			})
+        .state('enterprise.profile.edit', {
+          url: '/edit',
+          templateUrl: '/app/enterprise/profile/edit/enterprise-profile-edit.tmpl.html',
+          controller: 'EditEnterpriseCtrl as editEnterpriseCtrl'
+        });;
+  	}])
+    .controller('EditEnterpriseCtrl', ['$scope', function ($scope) {
+      
+      var editEnterpriseCtrl = this;
+
+
+    }]);
  
 
 })();
