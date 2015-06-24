9fd1c6b9bf6ff70774901b9232f5aa4d5946605b
John <john@odins-beard.com>
2015-06-24 09:40:08 -0400

    Time picker as directive

--- client/app/common/time-picker.directive.js
+++ client/app/common/time-picker.directive.js
@@ -9,9 +9,35 @@
 				restrict: "E",
 				templateUrl: 'app/common/time-picker.directive.html',
 				link: function (scope, el, attrs) {
-					console.log(attrs.name);
-
+					angular.extend(scope, {
+						"title": attrs.title,
+						"fieldName": attrs.name,
+						"hour": 12,
+						"minute": 0,
+						"median": "am",
+						"time": ""
+					});
 					
+					scope.adjustHours = function(adjTime){
+						scope.hour = scope.hour + adjTime > 12 ? 1 : (scope.hour + adjTime < 1 ? 12 : scope.hour + adjTime);
+						makeTime();
+					}
+					
+					scope.adjustMinutes = function(adjTime){
+						scope.minute = scope.minute + adjTime > 59 ? 0 : (scope.minute + adjTime < 0 ? 59 : scope.minute + adjTime);
+						makeTime();
+					}
+					
+					scope.adjustMedian = function(){
+						scope.median = scope.median == "am" ? "pm" : "am";
+						makeTime();
+					}
+					
+					function makeTime(){
+						scope["fieldName"] = ("00" + scope.hour).slice(-2) + ":" + ("00" + scope.minute).slice(-2) + " " + scope.median;
+					}
+					
+					makeTime();
 				}
 			};
 		}
