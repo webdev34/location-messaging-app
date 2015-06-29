03e257a64ec3fecdaa9c80d446e5aa117e7ecb87
John <john@odins-beard.com>
2015-06-29 12:50:20 -0400

    Update user service

--- client/app/common/services/user-service.js
+++ client/app/common/services/user-service.js
@@ -9,80 +9,25 @@
 		'API_URL',
 		
 		function($http, $q, API_URL) {
-			function extractData(response) {
-				return response.data.data;
-			}
-			
-			function extractError(response) {
-				return response.data;
-			}
-			
-			function validate(response){
-				return typeof response.data.data === 'object';
-			}
-
 			return {
 				login : function(userObj) {
-					return $http.post(API_URL + '/session', {'user' : userObj})
-						.then(
-							function(response) {
-								return validate(response) ? extractData(response) : $q.reject(extractError(response));
-							},
-							function(response) {
-								return $q.reject(extractError(response));
-							}
-						);
+					return $http.post(API_URL + '/session', {'user' : userObj});
 				},
 
 				logout : function(userObj) {
-					return $http.del(API_URL + '/session')
-						.then(
-							function(response) {
-								return validate(response) ? extractData(response) : $q.reject(extractError(response));
-							},
-							function(response) {
-								return $q.reject(extractError(response));
-							}
-						);
+					return $http.del(API_URL + '/session');
 				},
 
-				//*** doesn't exist yet
 				register : function(userObj) {
-					return $http.post(API_URL + '/register', {'user' : userObj})
-						.then(
-							function(response) {
-								return validate(response) ? extractData(response) : $q.reject(extractError(response));
-							},
-							function(response) {
-								return $q.reject(extractError(response));
-							}
-						);
+					return $http.post(API_URL + '/register', {'user' : userObj});
 				},
 
-				//*** doesn't exist yet
 				get : function(userId) {
-					return $http.get(API_URL + '/user/' + userId + '/user')
-						.then(
-							function(response) {
-								return validate(response) ? extractData(response) : $q.reject(extractError(response));
-							},
-							function(response) {
-								return $q.reject(extractError(response));
-							}
-						);
+					return $http.get(API_URL + '/user/' + userId + '/user');
 				},
 
-				//*** doesn't exist yet
 				update : function(userId, userObj) {
-					return $http.post(API_URL + '/user/' + userId + '/user', {'user' : userObj})
-						.then(
-							function(response) {
-								return validate(response) ? extractData(response) : $q.reject(extractError(response));
-							},
-							function(response) {
-								return $q.reject(extractError(response));
-							}
-						);
+					return $http.post(API_URL + '/user/' + userId + '/user', {'user' : userObj});
 				}
 			}
 		}
