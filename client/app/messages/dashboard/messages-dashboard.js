295f1bf83c6748f9f1c874815f737e10fbbf8d11
John <john@odins-beard.com>
2015-06-30 13:43:28 -0400

    Switch sid to _id to pull correct data into messages detail controller

--- client/app/messages/dashboard/messages-dashboard.js
+++ client/app/messages/dashboard/messages-dashboard.js
@@ -3,6 +3,7 @@
 
 	angular.module('messages.dashboard', [])
 
+	/*
 	.controller('ActiveMessageController', [
 		'$scope',
 		'$http',
@@ -13,17 +14,14 @@
 			$http,
 			MessageListModel
 		) {
-					console.log("Active Messages");
 			MessageListModel.getMessageList().then(
 				function(response){
-					console.log("------>", response);
 					$scope.active = response;
 				}
 			);
 		}
 	])
 
-	/*
 	.controller('LiveFeedController', ['$scope', '$http', function($scope, $http) {
 		$http.get('assets/data/live-feed.json').
 		success(function(data) {
