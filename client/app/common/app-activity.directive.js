9f7661c656dbae709e2144f4250d87f47702e96e
david balogun <abalogun@spinsys.com>
2015-08-31 15:00:58 -0400

    Overall actvity summary data model added

--- client/app/common/app-activity.directive.js
+++ client/app/common/app-activity.directive.js
@@ -13,3 +13,23 @@
 	);
 
 })();
+
+
+(function() {
+	'use strict';
+
+	angular.module('enterprise-portal')
+	.controller('OverAllActivityCtrl', [
+		'$state',
+		'$scope',
+		'$http',
+		function($state, $scope, $http) {
+			var overAllActivityCtrl = this;
+			$http.get('assets/data/overall-activity.json').success(function(data) {
+				$scope.overallActivityFeed = data.overallActivity;
+			});
+			
+		}
+	]);
+
+})();
