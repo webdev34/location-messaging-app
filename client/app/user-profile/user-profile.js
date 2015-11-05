7a09a18c255fe7edd49ddd5e08e29bb056505e48
Alex Karasyov <alex@karasyov.com>
2015-11-05 14:35:56 -0500

    Revert "Merge pull request #15 from QuiverMedia/web-summit_nov-3-2015"
    This reverts commit 3ff0de61cd3f0824ea27647428cacfbc685f1e58, reversing
    changes made to f8d0c776f46b03ed2f49107f88328a427fbafbd3.

--- client/app/user-profile/user-profile.js
+++ client/app/user-profile/user-profile.js
@@ -21,7 +21,6 @@
 		
 		var userProfileCtrl = this;
 		userProfileCtrl.isEditing = false;
-		userProfileCtrl.imageUploaded = false;
 
 
 		function init() {
@@ -29,38 +28,37 @@
 		}
 
 		function getAccount() {
-			// UserModel.getAccount(UserModel.user.sid)
-			// 	.then(
-			// 		function success (response) {
-			// 			console.log('response: ' + response);
-			// 			userProfileCtrl.user = UserModel.user;
-			// 			userProfileCtrl.editedUser = userProfileCtrl.user;
+			UserModel.getAccount(UserModel.user.sid)
+				.then(
+					function success (response) {
+						console.log('response: ' + response);
+						userProfileCtrl.user = UserModel.user;
+						userProfileCtrl.editedUser = userProfileCtrl.user;
 
-			// });
+			});
 
-			userProfileCtrl.user = {
-				'_id': '',
-				"companyName": "Quiver",
-				"firstName": "David",
-				"lastName": "Ball",
-				"logo": "assets/img/default-profile-avatar.png",
-				"email": "david.ball@quiver.com",
-				"phone": "7187187188",
-				"userRights": "Admin"
-			}
+			// userProfileCtrl.user = {
+			// 	'_id': '',
+			// 	"companyName": "TESLA Motors",
+			// 	"firstName": "Jason",
+			// 	"lastName": "Tumbler",
+			// 	"logo": "assets/img/default-profile-avatar.png",
+			// 	"email": "jtumbler@teslamotors.com",
+			// 	"phone": "7187187188",
+			// 	"userRights": "Admin"
+			// }
 		}
 
 
 		$scope.uploader = {};
 
 	  	$scope.processFiles = function(files){
-	  		userProfileCtrl.imageUploaded = true;
+	  		userProfileCtrl.user.newlogo = null;
 	    	angular.forEach(files, function(flowFile, i){
 	       	var fileReader = new FileReader();
 	          	fileReader.onload = function (event) {
 	            	var uri = event.target.result;
 	            	userProfileCtrl.user.newlogo = uri;
-	            	userProfileCtrl.user.newlogoPlacerHolder = angular.copy(userProfileCtrl.user.newlogo);
 	              	// userProfileCtrl.user.newlogo.push(uri);
 	          	};
 	          	fileReader.readAsDataURL(flowFile.file);
