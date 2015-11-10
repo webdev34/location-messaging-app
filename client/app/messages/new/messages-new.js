2cafd2e4449ac67ad80a788f6d2a0e35cce6b63b
david balogun <abalogun@spinsys.com>
2015-11-10 13:46:36 -0500

    Completed radius slider directive

--- client/app/messages/new/messages-new.js
+++ client/app/messages/new/messages-new.js
@@ -181,19 +181,19 @@
 		        $scope.uploader.flow.files.splice(index, 1);
 		  	};
 
-			$scope.map_range_change = function(operator) {
-				var currentRange = parseFloat($scope.newMessageCtrl.newMessage.range);
-				if(operator == 'addRange'){
-					if(currentRange != 100){
-					  	$scope.newMessageCtrl.newMessage.range = currentRange + 0.50;
-					}
-				}
-				else{
-					if(currentRange != 0){
-						$scope.newMessageCtrl.newMessage.range = currentRange - 0.50;
-					}
-				}
-			};
+			// $scope.map_range_change = function(operator) {
+			// 	var currentRange = parseFloat($scope.newMessageCtrl.newMessage.range);
+			// 	if(operator == 'addRange'){
+			// 		if(currentRange != 100){
+			// 		  	$scope.newMessageCtrl.newMessage.range = currentRange + 0.50;
+			// 		}
+			// 	}
+			// 	else{
+			// 		if(currentRange != 0){
+			// 			$scope.newMessageCtrl.newMessage.range = currentRange - 0.50;
+			// 		}
+			// 	}
+			// };
 			
 			function clearTakeOverSelectors(){
 				newMessageCtrl.showStartDatePicker = false;
