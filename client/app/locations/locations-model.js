ea556c0790248766a925662372ea30897bb69a5b
Alex Karasyov <alex@karasyov.com>
2015-11-16 13:13:38 -0500

    cleanip

--- client/app/locations/locations-model.js
+++ client/app/locations/locations-model.js
@@ -43,6 +43,7 @@
 						function success(response) {
 							var locObj = response.location[0];
 							var formattedLocation = {
+								"sid": locObj.sid,
 								"name": locObj.name,
 								"range": locObj.distance / 1000,
 								"discoverOn": locObj.trigger,
@@ -73,11 +74,11 @@
 				return LocationsService.post(formattedLocation)
 					.then(
 						function success(response){
-							console.log('success from model')
+							//console.log('success from model')
 							return response;
 						},
 						function error(response) {
-							console.log('error from model')
+							//console.log('error from model')
 							return response;
 						});
 
@@ -96,8 +97,29 @@
 						});
 			}
 
-			model.updateLocation = function(updatedLocation) {
-				
+			model.updateLocation = function(newLocation) {
+				var formattedLocation = {
+					"location": {
+						"sid": newLocation.sid,
+						"name": newLocation.name,
+						"address": newLocation.address,
+						"latitude": newLocation.coordinates.lat,
+						"longitude": newLocation.coordinates.lng,
+						"distance": newLocation.range*1000,
+						"trigger": newLocation.discoverOn
+					}	
+				}
+
+				return LocationsService.post(formattedLocation)
+					.then(
+						function success(response){
+							//console.log('success from model')
+							return response;
+						},
+						function error(response) {
+							//console.log('error from model')
+							return response;
+						});
 			}
 
 		
